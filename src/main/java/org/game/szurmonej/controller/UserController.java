5098c778c7bb32a8114cca8c5c77c6fa4b95e8ce
maxwell <246339@edu.p.lodz.pl>
2026-05-24 16:24:03 +0200

    Database init

---
+++ src/main/java/org/game/szurmonej/controller/UserController.java
@@ -0,0 +1,29 @@
+package org.game.szurmonej.controller;
+
+import org.game.szurmonej.entity.User;
+import org.game.szurmonej.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userRepository.save(user));
+    }
+}
